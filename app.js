0951d27f0eecfa44bf5116243781ad9e109b7e2b
hdraptorr <nascan61.@gmail.com>
2021-11-18 15:11:55 +0300

    stick-nav

--- app.js
+++ app.js
@@ -14,4 +14,38 @@ sidebarElements.forEach((sideNavBtn)=>
   })
 );
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+
+// sticky navbar after scrolling in the amount of margintop
+
+window.onscroll = function() {stickyNav()}
+
+function stickyNav(){
+
+var header = document.getElementById("header");
+var container = document.getElementById("contain")
+var sticky = container.offsetTop;
+
+if(window.scrollY >= sticky){
+  header.classList.add("sticky")
+}else{
+  header.classList.remove("sticky")
+}
+
+}
 
